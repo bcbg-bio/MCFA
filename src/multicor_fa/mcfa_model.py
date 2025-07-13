5c5d51fe050bda85e01a87045e5ca8ebab428090
sherxie <sxie04@gmail.com>
2025-07-13 17:57:52 -0400

    fix any(df) issue via daniel

--- src/multicor_fa/mcfa_model.py
+++ src/multicor_fa/mcfa_model.py
@@ -262,7 +262,7 @@ def fit(Y: Iterable[pd.DataFrame], n_pcs: Union[str, List[int]] = 'infer',
     sample_names = [Y_m.index for Y_m in Y]
     feature_names = [Y_m.columns for Y_m in Y]
 
-    if any(any(pd.isna(Y_m)) for Y_m in Y):
+    if any(Y_m.isna().any(axis = None) for Y_m in Y):
         if missing_entries == 'raise':
             raise ValueError('Missing entries detected in some datasets')
         elif missing_entries == 'drop':
